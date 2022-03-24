538e46d76fcad8c01d5b034fee30334773946865
ecorellar <ecorellar.ux@gmail.com>
2022-03-24 18:33:12 -0400

    adding Cafe definition

--- src/main/java/com/coffeeshop/app/Cafe.java
+++ src/main/java/com/coffeeshop/app/Cafe.java
@@ -1,1 +1,52 @@
 package com.coffeeshop.app;
+
+public final class Cafe {
+
+	private int beansInStock = 0;
+	private int milkIntStock = 0;
+
+	public Coffee brew(CoffeeType coffeeType) {
+
+		return brew(coffeeType, 1);
+	}
+
+	public Coffee brew(CoffeeType coffeeType, int quantity) {
+
+		requirePositive(quantity);
+
+		int requiredBeans = coffeeType.getRequiredBeans() * quantity;
+		int requiredMilk = coffeeType.getRequiredMilk() * quantity;
+
+		if (requiredBeans > beansInStock || requiredMilk > milkInStock) {
+			throw new IllegalStateException();
+		}
+
+		beansInStock -= requiredBeans;
+		milkInStock -= requiredMilk;
+		return new Coffee(coffeeType, requiredBeans, requiredMilk);
+	}
+
+	public void restockBeans(weightInGrams) {
+		requirePositive(weightInGrams);
+		beansInStock += weightInGrams;
+	}
+
+	public void restockMilk(weightInGrams) {
+		requirePositive(weightInGrams);
+		milkInStock += wightInGrams;
+	}
+
+	private void requirePositive(int value) {
+		if (value < 1) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public int getBeansInStock() {
+		return beansInStock;
+	}
+
+	public int getMilkInStock() {
+		return milkInStock;
+	}
+}
